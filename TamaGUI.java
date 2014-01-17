87a01f12b2c52aa1d6a98a69a1c0a9987134ff7b
Andreas <andreasfoghp@hotmail.com>
2014-01-17 22:30:07 +0100

    Primitive version of GUI

--- TamaGUI.java
+++ TamaGUI.java
@@ -1,1 +1,21 @@
-public class TamaGUI {}
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class TamaGUI extends JApplet{
+
+	public void init() {
+
+		final JLabel values = new JLabel();
+		final JLabel world = new JLabel();
+		final JButton eat = new JButton("Eat");
+		final JButton sleep = new JButton("Sleep");
+
+		setLayout(new GridLayout(3,2));
+		add(values);
+		add(world);
+		add(eat);
+		add(sleep);
+	}
+}
