0f2767c3935773b6cfc51933e83251372aef1b41
Gitnone <mortenvemmelund@gmail.com>
2014-01-18 22:46:13 +0100

    Test Shared Model

--- HungryGotchi.java
+++ HungryGotchi.java
@@ -5,7 +5,7 @@ public class HungryGotchi extends Tamagotchi {
     private int energy = 20;
 
     public void eat() {
-        if (hunger == 20) {
+        if (hunger == 25) {
             System.out.println("I'm full!");
         }
         else {
