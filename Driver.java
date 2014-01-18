0f644a74107ee59c87cded87e7b3bba2803585c4
Gitnone <mortenvemmelund@gmail.com>
2014-01-18 15:52:43 +0100

    Backend structure

---
+++ Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args) {
+        Tamagotchi hungry = new HungryGotchi();
+        Tamagotchi sleepy = new SleepyGotchi();
+
+        sleepy.sleep();
+
+        sleepy.getIsSleeping();
+        hungry.getIsSleeping();
+    }
+}
