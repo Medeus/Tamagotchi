5b923405590fe74368a7a93fc1c4742f108cca73
Andreas <andreasfoghp@hotmail.com>
2014-01-22 15:04:01 +0100

    Small fix in eat()-method in YodaGotchi
    It prints "I'm full" right away and not after 1-2 clicks.

--- YodaGotchi.java
+++ YodaGotchi.java
@@ -1,13 +1,12 @@
 public class YodaGotchi extends Tamagotchi {
     public void eat() {
+        hunger += 5;
         if (hunger == maxHunger) {
             System.out.println("I'm full!");
         }
         else if (hunger + 5 >= maxHunger) {
             hunger = maxHunger;
-        }
-        else {
-            hunger += 5;
+            System.out.println("I'm full!");
         }
     }
 
