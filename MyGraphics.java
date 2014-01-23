946c717c9aa42f4aa70bfdce37eca70314e545c3
Gitnone <Gitnone@users.noreply.github.com>
2014-01-23 02:51:39 +0100

    Create MyGraphics.java
    Readded since it was being use to change image during runtime.

---
+++ MyGraphics.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.Timer;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Image;
+import java.util.*;
+import javax.imageio.*;
+import java.net.*;
+import java.applet.*;
+
+public class MyGraphics extends JComponent {
+	private Image tamagotchi;
+
+	public MyGraphics(Image i) {
+		Image tamagotchi = i;
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(tamagotchi, 20, 40, tamagotchi.getHeight(this), tamagotchi.getWidth(this), this);
+	}
+}
